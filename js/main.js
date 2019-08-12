1f7b255a72d2c4d221df27ef6fc27913464ad81e
0001SID <mailofsumandas@gmail.com>
2019-08-12 12:36:22 +0530

    2 option

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ function addQuestion() {
         answer: document.getElementById('answer').value.trim(),
         explanation: document.getElementById('explanation').value.trim()
     };
-    if(data.chapter != '' && data.op1 != '' && data.op2 != '' && data.op3 != '' && data.op4 != ''){
+    if(data.chapter != '' && data.op1 != '' && data.op2 != ''){
     // if (isNull(data.chapter) || isNull(data.op1) || isNull(data.op2) || isNull(data.op) || isNull(data.op4)) {
         
         $.ajax({
